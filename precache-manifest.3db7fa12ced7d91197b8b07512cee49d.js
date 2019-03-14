104549d537cc43788fd25e2c936936b3ac19a22c
ufufuru <obadere@gmail.com>
2019-03-14 17:07:49 -0500

    Updates

---
+++ precache-manifest.3db7fa12ced7d91197b8b07512cee49d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf5a1701a4541b353ea5525c1b64e123",
+    "url": "/dallasdoubledoodles/static/media/wallpaper.bf5a1701.jpeg"
+  },
+  {
+    "revision": "b76787987b478332913d76a9ef14dc8c",
+    "url": "/dallasdoubledoodles/static/media/curls.b7678798.png"
+  },
+  {
+    "revision": "619e5f2efb49987cb918",
+    "url": "/dallasdoubledoodles/static/js/runtime~main.619e5f2e.js"
+  },
+  {
+    "revision": "29e47d1eefe9764b722c",
+    "url": "/dallasdoubledoodles/static/js/main.29e47d1e.chunk.js"
+  },
+  {
+    "revision": "518f773fc454461cb03e",
+    "url": "/dallasdoubledoodles/static/js/2.518f773f.chunk.js"
+  },
+  {
+    "revision": "29e47d1eefe9764b722c",
+    "url": "/dallasdoubledoodles/static/css/main.51bbb59a.chunk.css"
+  },
+  {
+    "revision": "518f773fc454461cb03e",
+    "url": "/dallasdoubledoodles/static/css/2.be7a806d.chunk.css"
+  },
+  {
+    "revision": "2661d8b78cf25c5df29c8b28559347e9",
+    "url": "/dallasdoubledoodles/index.html"
+  }
+];
