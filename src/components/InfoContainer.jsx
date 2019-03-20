f999e02eadc38a97f0a85a995c10e4172823ee71
ufufuru <obadere@gmail.com>
2019-03-20 06:46:42 -0500

    Changed wallpaperp

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -16,11 +16,11 @@ function InfoContainer() {
                                 What are double
                                 doodles? They are cross between a labradoodle and golden-doodle. A labradoodle is a cross between a
                                 poodle which can be standard or miniature. All of our doodles will be medium to large in size ranging
-                                from 50-70 pounds. A golden-doodle is a cross between a poodle and gold retriever. Poodles remain the
+                                from 50-70 pounds. A golden-doodle is a cross between a poodle and golden retriever. Poodles remain the
                                 second smartest dog in the purebred world along with being very loyal and protective, while Labrador
-                                and Golden Retrievers remain the most popular pure bred dogs because their intelligence, athleticism,
+                                and Golden Retrievers remain the most popular pure bred dogs because of their intelligence, athleticism,
                                 and being family friendly dogs. With double doodles, you get double the love and fun along with being
-                                allergy friendly with low to non-shedding coats along with low to no odor pet dander. They are also
+                                allergy friendly with low to non-shedding coats along with low to no odor or pet dander. They are also
                                 gentle, easily trainable, smart, loyal and athletic making them the perfect family dog.
                             </p>
                         </div>
